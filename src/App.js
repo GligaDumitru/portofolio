3a326d9230bb22e9751b931ec92e22b4b7ee2d34
GligaDumitru <gliga_dumitru@yahoo.com>
2021-04-15 23:46:10 +0300

    changes for setting the deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <h1>Gliga Dumitru Portofolio 10</h1>
+        <h1>PORTOFOLIO GLIGA</h1>
       </header>
     </div>
   );
